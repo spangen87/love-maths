2a30ae2ca95f6a74a39a3333a26644574fe4b1d6
spangen87 <rikard.spangmyr@bestlas.se>
2022-05-19 13:01:58 +0000

    Add code for division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,7 +44,8 @@ function runGame(gameType) {
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
     } else if (gameType === "division") {
-        displayDivisionQuestion(num1, num2);    }
+        displayDivisionQuestion((num1 * num2), num2);    
+    }
     else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
